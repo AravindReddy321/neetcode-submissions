e7a0fed3df05a64e6fc584fa10abf7a65be6080e
AravindReddy321 <119138372+AravindReddy321@users.noreply.github.com>
2026-04-24 18:03:40 -0500

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+
+        //return hashTable(numbers,target);
+        return twoPointer(numbers, target);
+        
+    }
+
+    public int[] twoPointer(int[] nums, int t){
+        int l=0,r=nums.length-1;
+        while(l<r){
+            int sum = nums[l]+nums[r];
+            if(sum > t){
+                r--;
+            }else if(sum < t){
+                l++;
+            }else{
+                return new int[] {l+1,r+1};
+                
+            }
+        }
+        return new int[0];
+    }
+
+    public int[] hashTable(int[] numbers,int target){
+        Map<Integer, Integer> m = new HashMap<>();
+        int[] res = new int[2];
+        for(int i=0;i<numbers.length;i++){
+            if(m.containsKey(numbers[i])){
+                res[0] =m.get(numbers[i]);
+                res[1] =i+1;
+                return res;
+            }
+            m.put(target-numbers[i],i+1);
+        }
+        return res;
+    }
+}
