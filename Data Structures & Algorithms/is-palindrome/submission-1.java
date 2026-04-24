388273bac1bc21f750245cdd4eb20836356ea6be
AravindReddy321 <119138372+AravindReddy321@users.noreply.github.com>
2026-04-23 19:11:40 -0500

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,68 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        //return reverseString1(s);
+        // return reverseString2(s);
+        // return twoPointer(s);
+        return twoPointer2(s);
+    
+            
+        
+    }
+
+    public boolean twoPointer2(String s){
+        int l=0,r=s.length()-1;
+        while(l<r){
+            while(l<r && !Character.isLetterOrDigit(s.charAt(l))){
+                l++;
+            }
+            while(l<r && !Character.isLetterOrDigit(s.charAt(r))){
+                r--;
+            }
+            if(Character.toLowerCase(s.charAt(l)) != Character.toLowerCase(s.charAt(r)))
+            {
+                return false;
+            }
+            l++;r--;
+        }
+        return true;
+    }
+
+    public boolean twoPointer(String s){
+        for(int i=0,j=s.length()-1;i<j;){
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+                continue;
+            }
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+            char low = Character.toLowerCase(s.charAt(i));
+            char high = Character.toLowerCase(s.charAt(j));
+            if(low != high) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public boolean reverseString2(String s){
+        StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()){
+            if(Character.isLetterOrDigit(c))
+                sb.append(Character.toLowerCase(c));
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
+
+    public boolean reverseString1(String s){
+        StringBuilder sb = new StringBuilder();
+        s=s.toLowerCase();
+        for(int i=0;i<s.length();i++){
+            if(Character.isLetterOrDigit(s.charAt(i)))
+                sb.append(s.charAt(i));
+        }
+        return sb.toString().equals(sb.reverse().toString());
+    }
+}
