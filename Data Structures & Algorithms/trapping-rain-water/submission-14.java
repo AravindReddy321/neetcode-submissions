534963bee2ee8dbd092e5fb6569838dfa855e59d
AravindReddy321 <119138372+AravindReddy321@users.noreply.github.com>
2026-04-25 19:22:32 -0500

    Add: trapping-rain-water - submission-14

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-14.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int trap(int[] height) {
+        return twoPointer(height);
+    }
+
+    int twoPointer(int[] height){
+        int[] prefix = new int[height.length];
+        int[] suffix = new int[height.length];
+        int res=0;
+
+        int lmax=height[0];
+        // prefix[0]=lmax;
+        for(int i=0;i<height.length;i++){
+            lmax=Math.max(height[i],lmax);
+            prefix[i]=lmax;
+        }
+
+        int rmax=height[height.length-1];
+        //suffix[height.length-1]=rmax;
+        for(int i=height.length-1;i>=0;i--){
+            rmax=Math.max(height[i],rmax);
+            suffix[i]=rmax;
+        }
+        
+        for(int i=0;i<height.length;i++){
+            int area = Math.min(prefix[i], suffix[i])-height[i];
+            res += Math.max(area,0);
+        }
+        return res;
+    }
+}
