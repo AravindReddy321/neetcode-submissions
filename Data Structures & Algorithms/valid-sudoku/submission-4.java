15bd4ef43ea4156f14c946128dac1eb7e6be1c05
AravindReddy321 <119138372+AravindReddy321@users.noreply.github.com>
2026-05-02 20:06:43 -0500

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        for(int i=0;i<board.length;i++){
+            Set<Character> rset = new HashSet<Character>();
+            for(int j=0;j<board.length;j++){
+                if(board[i][j]=='.') continue;
+                if(rset.contains(board[i][j])) { print("rowset");return false;}
+                rset.add(board[i][j]);
+            }
+        }
+        for(int i=0;i<board.length;i++){
+            Set<Character> cset = new HashSet<Character>();
+            for(int j=0;j<board.length;j++){
+                if(board[j][i]=='.') continue;
+                if(cset.contains(board[j][i])) { print("colset");return false;}
+                cset.add(board[j][i]);
+            }
+        }
+        for(int i=0;i<board.length;i=i+3){
+            for(int j=0;j<board.length;j=j+3){
+                Set<Character> sqset = new HashSet<>();
+                for(int r=i;r<i+3;r++){
+                    for(int c=j;c<j+3;c++){
+                        if(board[r][c]=='.') continue;
+                        if(sqset.contains(board[r][c])) { print("square board"+r+" "+c);return false;}
+                        sqset.add(board[r][c]);
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    void print(String s){
+        System.out.println(s);
+    }
+    
+}
